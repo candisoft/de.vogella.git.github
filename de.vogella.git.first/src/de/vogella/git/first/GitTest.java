02b3d977104c4f2f80bc104c77913b512266f6d5
CANDI <CANDI@PC226-candi.arranzacinas.es>
2012-11-24 18:17:46 +0100

    Commit Rama 1.5

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest {
 	  public static void main(String[] args) {
-	    System.out.println("git Rama1");
+	    System.out.println("git RAMA 1.5");
 	  }
 }
